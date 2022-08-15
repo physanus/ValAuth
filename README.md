db2189e7f26357b62cd933b63f71de1c1e9e65c9
physanus <physanus@users.noreply.github.com>
2022-08-15 11:56:36 +0200

    Add initial version of the code

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # ValAuth
 An auth provider for use in bash scripts
+
+# Motivation
+So far, I only found [this](https://github.com/HeyM1ke/ValorantClientAPI/blob/master/Docs/RSO_AuthFlow.py) 
+authentication wrapper that is capable of generating an `access_token`, `entitlements_token` and `user_id`. However, 
+that script broke in the meantime and was still broken after applying
+[some adjustments](https://github.com/HeyM1ke/ValorantClientAPI/issues/28#issuecomment-1059647354).
+
+[Other projects](https://github.com/staciax/Valorant-DiscordBot/issues/57) do suffer from this, too.
+
+As a result, I looked into this issue and found [a solution on Stack Overflow](https://stackoverflow.com/a/49088162).
+The problem seems to be a (recently introduced) recently added security-measure by Cloudflare on Riots servers:
+```
+This is due to the fact that the page uses Cloudflare's anti-bot page (or IUAM).
+Bypassing this check is quite difficult to solve on your own, since Cloudflare changes their techniques periodically.
+Currently, they check if the client supports JavaScript, which can be spoofed.
+```
+[source](https://stackoverflow.com/a/49088162)
+
+Also:
+```
+[The cloudscraper] does not require Node.js dependency
+```
+[source](https://stackoverflow.com/a/60884613)
+
+This projects implements the changes on top of the authentication scheme provided by both 
+[ValorantClientAPI](ValorantClientAPI) and [Postman](https://www.postman.com/flight-astronomer-35971560/workspace/riot-auth/documentation/19680348-ebef585d-d9a4-42ad-a33b-e0a3e67e0d08).
+
+# Prerequisites
+- You need to have 2fa **disabled** on your Riot Account ([click here](https://account.riotgames.com/#mfa-card) to do so).
+- You need wither python or docker installed on your system.
+
+# How to run
+## Vanilla
+```
+$ pip3 install -r requirements.txt
+$ VAL_USER=username VAL_PASS=password python3 auth.py
+```
+
+## Docker
+```
+docker run \
+  --rm \
+  -it \
+  -e VAL_USER='physanus' \
+  -e VAL_PASS='9EW&jS#YR*64R1uU^0@Um@@Ka@K5' \
+  --entrypoint ash \
+  -v "$(pwd):/app" \
+  python:3-alpine \
+  -c 'cd /app && pip3 install -r requirements.txt >/dev/null 2>&1 && python3 auth.py'
+```
+
---
+++ auth.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import cloudscraper
+import json
+import os
+import re
+
+
+def auth(username, password):
+    # print(f'Initiating auth for user \'{username}\' pass \'{password}\'')
+
+    scraper = cloudscraper.create_scraper()
+    # scraper.debug = True
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
+        'Content-Type': 'application/json',
+    }
+
+    # Request 1 - Setup cookies
+    data = {
+        'client_id': 'play-valorant-web-prod',
+        'nonce': '1',
+        'redirect_uri': 'https://playvalorant.com/opt_in',
+        'response_type': 'token id_token',
+    }
+    result = scraper.post('https://auth.riotgames.com/api/v1/authorization', headers=headers, json=data)
+    if result.status_code != 200:
+        print('Error in request 1: ' + str(result.status_code))
+        exit(2)
+
+    # Request 2 - Retrieve the access_token
+    data = {
+        'type': 'auth',
+        'username': username,
+        'password': password,
+    }
+    result = scraper.put('https://auth.riotgames.com/api/v1/authorization', headers=headers, json=data)
+    if result.status_code != 200:
+        print('Error in request 1: ' + str(result.status_code))
+        exit(3)
+
+    re_pattern = re.compile(
+        'access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
+    try:
+        re_data = re_pattern.findall(json.loads(result.text)['response']['parameters']['uri'])[0]
+    except KeyError:
+        print('Error in request 1: ' + str(result.status_code))
+        exit(4)
+    access_token = re_data[0]
+    id_token = re_data[1]
+    expires_in = re_data[2]
+    print(f'access_token={access_token}')
+    headers['Authorization'] = f'Bearer {access_token}'
+
+    # Request 3 - Retrieve the entitlements_token
+    result = scraper.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers)
+    if result.status_code != 200:
+        print('Error in request 1: ' + str(result.status_code))
+        exit(5)
+
+    entitlements_token = json.loads(result.text)['entitlements_token']
+    print(f'entitlements_token={entitlements_token}')
+    headers['X-Riot-Entitlements-JWT'] = entitlements_token
+
+    # Request 4 - Retrieve the user_id
+    result = scraper.post('https://auth.riotgames.com/userinfo', headers=headers)
+    if result.status_code != 200:
+        print('Error in request 1: ' + str(result.status_code))
+        exit()
+
+    user_id = json.loads(result.text)['sub']
+    print(f'user_id={user_id}')
+
+
+if __name__ == '__main__':
+    username = os.getenv('VAL_USER')
+    password = os.getenv('VAL_PASS')
+
+    if not username or not password:
+        print("Error: You need to specify the env variables 'VAL_USER' and 'VAL_PASS'.")
+        exit(1)
+
+    auth(username, password)
